d0eb3d51e219a94522b6b647cbc49fca4bbc1272
FeriCodeDummy <jan.zajc3@student.um.si>
2025-05-14 13:31:37 +0200

    Upgraded model from vosk to openai's whisper - faster, way more accurate, 10x smaller than VOSK. Also added simple cors

--- server.py
+++ server.py
@@ -1,16 +1,15 @@
 from flask import Flask, request, jsonify
-from vosk import Model, KaldiRecognizer
-import wave
 import io
-from pydub import AudioSegment
 import json
 import time
-from speech_to_text import stt
-from datetime import datetime
 import mysql.connector
 import os
 from dbm import save_transcribed
 from dotenv import load_dotenv, dotenv_values 
+import whisper
+import tempfile
+from flask_cors import CORS
+
 load_dotenv() 
 get = os.getenv
 
@@ -18,18 +17,6 @@ HOST = get("MYSQL_HOST")
 USER = get("MYSQL_USER")
 PSWD = get("MYSQL_PASSWORD")
 PORT = int(get("MYSQL_PORT"))
-VOSK_MODEL = "vosk-model-en-us-0.22"
-
-
-print(f"[*] Loading model {VOSK_MODEL}")
-try:
-    s = time.time()
-    model = Model(VOSK_MODEL)
-    e = time.time()
-    print(f"[+] Loaded Vosk model in {e-s}s")
-except Exception as e:
-    print("[!] Vosk model failed to load. Quitting...")
-    exit(-1)
 
 print("[*] Connecting to MySQL database ...")
 try:
@@ -44,22 +31,31 @@ try:
 except Exception as e:
     print("[!] Failed to connect to the database. Quitting...")
     exit(-1)
+    
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/upload', methods=['POST'])
-def upload_audio():
+@app.route('/transcribe', methods=["POST"])
+def transcribe_audio():
     if 'audio_file' not in request.files:
         return jsonify({"error": "Missing 'audio_file'"}), 400
+    uploaded_file = request.files['audio_file']
 
-    uploaded_file = request.files['audio_file'].stream
-    suf = datetime.now().strftime("%d_%m_%Y_%H_%M_%S")
-    filename = f"audio_{suf}.wav"
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".mp3") as temp:
+        uploaded_file.save(temp.name)
+        temp_path = temp.name
 
-    
-    text = stt(uploaded_file, filename, model)
-    save_transcribed(database, text)
-    return jsonify({"transcript": text}), 200
+    try:
+        model = whisper.load_model('base')
+        result = model.transcribe(temp_path, language='en')
+        transcription = result['text']
+        save_transcribed(database, transcription)
+
+    finally:
+        os.remove(temp_path)  
+
+    return jsonify({"transribtion": transcription}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
